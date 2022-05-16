98862d63506835d9cb9b03296d2fde7af71cac36
a.kazakov <a.kazakov@bidzaar.com>
2022-05-16 23:22:31 +0300

    Step 8 - DZ

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,14 +4,14 @@ from django.urls import re_path
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     
+    re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
 
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailViews.as_view(), name='book-detail'),
 
     re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrower'),
-    
-    re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    re_path(r'^borroweds/$', views.LoanedAllBooksBorrowed.as_view(), name='borroweds'),
 ]
 
 # Примечание: В качестве дополнительного задания, рассмотрите возможность того, как вы могли бы закодировать url на список всех книг, вышедших в определённый год, месяц, день и какое РВ (паттерн) должно соответствовать этому.
--- catalog/views.py
+++ catalog/views.py
@@ -79,3 +79,11 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
     
+
+class LoanedAllBooksBorrowed(LoginRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_book.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o')
